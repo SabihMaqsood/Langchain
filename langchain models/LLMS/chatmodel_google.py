b7a446c82a35325aea30b17b0ecb178c99ba48c7
Muhammad Sabih Maqsood <f24-bscs-1098@uo.edu.pk>
2026-08-06 22:32:40 +0500

    Delete langchain models/LLMS directory

--- langchain models/LLMS/chatmodel_google.py
+++
@@ -1,9 +0,0 @@
-from langchain_google_genai import GoogleGenerativeAI
-from dotenv import load_dotenv
-
-load_dotenv()
-model = GoogleGenerativeAI(model="gemini-3.5-flash")
-
-response = model.invoke("Write a poem about the ocean.")
-
-print(response)
